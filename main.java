95294c7b015922e340792811cf85b4262fbbe2f6
Finn Carver <77501622+orangehoneybee@users.noreply.github.com>
2021-05-21 17:35:18 -0600

    Update main.java

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main {
     }
     public static void main(String[] var0) {
         System.out.println("Hello, my name is Finn");
+        System.out.println("Hello Finn, it is you but making an edit on git hub because you are a team of one.");
     }
 }
